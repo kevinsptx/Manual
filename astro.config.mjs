66d87f21e71298ce765d1426cb30be00777642d5
kevinsptx <kevinsptx123@gmail.com>
2026-06-02 07:50:18 -0500

    miertda total

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kevinsptx.github.io/Manual/',
+  base: '/Manual/',
   vite: {
     plugins: [tailwindcss()]
   }
